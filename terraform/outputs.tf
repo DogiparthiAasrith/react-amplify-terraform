9321a74020e97e158f41362877e5d14ef1f5ad9e
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-12-27 12:05:34 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,8 @@ output "amplify_branch_url" {
   description = "URL for the deployed branch"
   value       = "https://${aws_amplify_branch.main.branch_name}.${aws_amplify_app.react_app.default_domain}"
 }
+
+output "amplify_service_role_arn" {
+  description = "IAM Role ARN for Amplify service"
+  value       = aws_iam_role.amplify_service_role.arn
+}
