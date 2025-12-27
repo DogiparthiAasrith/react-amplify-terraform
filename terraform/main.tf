fa3d5631e6d05c1c2efe38e4bf077a9989dea1c8
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-12-27 12:56:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,10 @@
-# Data source to get current AWS account ID
-data "aws_caller_identity" "current" {}
-
-# IAM Role for Amplify Service (separate from OIDC role)
-resource "aws_iam_role" "amplify_service_role" {
-  name = "${var.project_name}-amplify-service-role"
-
-  # Trust policy allowing Amplify service to assume this role
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowAmplifyAssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "amplify.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name        = "${var.project_name}-amplify-service-role"
-    Environment = var.environment
-  }
-}
-
-# IAM Policy for Amplify Service Role - Using AWS Managed Policy
-resource "aws_iam_role_policy_attachment" "amplify_admin" {
-  role       = aws_iam_role.amplify_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
-}
-
-# Wait for IAM role to propagate
-resource "time_sleep" "wait_for_iam" {
-  depends_on      = [aws_iam_role_policy_attachment.amplify_admin]
-  create_duration = "20s"
-}
-
 # AWS Amplify App
 resource "aws_amplify_app" "react_app" {
   name       = var.project_name
   repository = var.repository_url
 
-  access_token         = var.github_access_token
-  iam_service_role_arn = aws_iam_role.amplify_service_role.arn
-
-  depends_on = [time_sleep.wait_for_iam]
+  # GitHub personal access token
+  access_token = var.github_access_token
 
   # Build settings for React app
   build_spec = <<-EOT
